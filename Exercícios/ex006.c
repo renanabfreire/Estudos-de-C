c9277a82aeddef108a72910bde3412b9895164a7
Renan Abreu B. Freire <renanabf.1301@gmail.com>
2023-07-31 21:33:18 -0300

    ex006

---
+++ Exercícios/ex006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("Digite um número:");
+    scanf("%i", &n);
+
+    printf("Seu dobro é igual a %i e seu triplo igual a %i",
+    n*2, n*3);
+
+    return 0;
+}
