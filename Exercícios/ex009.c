ed6211f51ad7d10d8a419736984c832d60acd729
Renan Abreu B. Freire <renanabf.1301@gmail.com>
2023-08-01 10:39:39 -0300

    ex009

---
+++ Exercícios/ex009.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int q;
+
+    printf("Você deseja ver a tabuada de qual número?");
+    scanf("%i", &q);
+
+    printf("------------\n");
+    printf("%i x 1  = %i\n", q, q);
+    printf("%i x 2  = %i\n", q, q*2);
+    printf("%i x 3  = %i\n", q, q*3);
+    printf("%i x 4  = %i\n", q, q*4);
+    printf("%i x 5  = %i\n", q, q*5);
+    printf("%i x 6  = %i\n", q, q*6);
+    printf("%i x 7  = %i\n", q, q*7);
+    printf("%i x 8  = %i\n", q, q*8);
+    printf("%i x 9  = %i\n", q, q*9);
+    printf("%i x 10 = %i\n", q, q*10);
+
+    return 0;
+}
