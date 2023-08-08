a37d83ff8ade604e9c58c465a7b0b747cbc2e7bc
Renan Abreu B. Freire <renanabf.1301@gmail.com>
2023-08-07 22:37:43 -0300

    ex022

---
+++ Exercícios/ex022.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double dist=0;
+
+    printf ("Digite a distância em km: ");
+    scanf ("%lf", &dist);
+
+    dist=floor(dist);
+
+    if (dist <= 200)
+    {
+        printf ("A sua viagem custará R$%.2lf.\n", dist*0.5);
+    }
+    else
+    {
+        printf ("A sua viagem custará R$%.2lf.\n", dist*0.45);
+    }
+
+    return 0;
+}
