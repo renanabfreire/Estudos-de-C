4d229436277a398cb530aeddf4b6b4ac4dc37875
Renan Abreu B. Freire <renanabf.1301@gmail.com>
2023-08-07 22:34:06 -0300

    ex021

---
+++ Exercícios/ex021.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n=0;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%i", &n);
+
+    if(n%2==0)
+    {
+        printf("%i é par\n", n);
+    }
+    else
+    {
+        printf("%i é ímpar\n", n);
+    }
+
+    return 0;
+}
